1d0f673db67029ffe6929a24f5f131dc94512027
Puppak <pappakos0201@gmail.com>
2026-01-16 10:49:30 +0100

    bugfix nb2 és stílus

--- frontend/Frontend/src/main.jsx
+++ frontend/Frontend/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import Foldal from './components/Foldal.jsx'
+import LandingPage from './components/LandingPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Foldal />
+    <LandingPage />
   </StrictMode>,
 )
