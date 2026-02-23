d07c4033da822533b09720709783a99ebc793ba6
Németh Zalán B. <zalufox@gmail.com>
2026-02-23 19:08:37 +0100

    i made the top bar and im still woring on the drop down menu and the logo and the drop bar needs some styleing but im working on it

--- mobilAlk/BudgetMobilApp/App.js
+++ mobilAlk/BudgetMobilApp/App.js
@@ -1,17 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from 'react-native';
+import { Image } from 'react-native';
 
-export default function App() {
+const onPress = () => {
+  console.log('Button Pressed!');
+};
+const dropDown = () => {
+  console.log('DropDown Pressed!');
+}
+
+export default function App({ navigation }) {
   return (
     <>
       <View style={styles.container}>
         {" "}
-        {/* Top Bar */}
         <View style={styles.topBar}>
-          <image
-            source={require("")}
-          ></image>
-        </View>  
+          <TouchableOpacity style={styles.button} onPress={onPress}> 
+            <Image source={require('./assets/applogo.png')} style={styles.icon} />
+           </TouchableOpacity>
+          <Text style={styles.topBarText}>Budget App</Text>
+          <TouchableOpacity style={styles.dropbox} onPress={dropDown}>
+            <Text style={styles.dropbar}>...</Text>
+          </TouchableOpacity>
+          
+        </View> 
+      
       </View>
     </>
   );
@@ -20,15 +34,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: { flex: 1 },
   topBar: {
-    height: 70,
+    flexDirection: "row",
+    height: 100,
     backgroundColor: "#1e90ff",
-    justifyContent: "flex-start",
+    justifyContent: "center",
     alignItems: "center",
-    paddingTop: 30,
-    borderRadius: 10,
+    paddingRight: 30,
+    paddingTop: 35,
     borderColor: "#000000",
-    borderWidth: 1,
+    borderBottomWidth: 3,
   },
-  topBarText: { color: "white", fontSize: 20, fontWeight: "bold" },
+  topBarText: { color: "white", fontSize: 20, fontWeight: "bold", flex: 2, paddingLeft: 20 },
   content: { flex: 1, justifyContent: "center", alignItems: "center" },
+  icon: { width: 50, height: 50, margin: 5, backgroundColor: "rgba(0, 82, 163, 0.5)", borderRadius: 10 },
+  button: {
+    marginLeft: 20,
+    borderRadius: 15,
+    flex: 1,
+  },
+  dropbar: 
+  {
+    
+  },
+  dropbox:
+  {
+    backgroundColor: "rgba(0, 82, 163, 0.5)",
+    borderRadius: 10,
+    paddingTop: 5,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 10,
+
+  }
 });
