2edb02f1ab372e19b2cc834074915ab63fbfbea1
Gabo <beleczky.gabor@liveedu.mathiasz.hu>
2026-03-13 11:58:58 +0100

    Dokumentáciok + valami

--- Backend/database/migrations/2026_03_09_020000_create_contact_tipusoks_table.php
+++ Backend/database/migrations/2026_03_09_020000_create_contact_tipusoks_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('contact_tipusok', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->id();
             $table->string('megnevezes');
         });
