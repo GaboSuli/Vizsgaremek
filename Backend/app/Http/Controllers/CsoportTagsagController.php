811380196a11a353a9f7cc543f001373be5352ec
GaboSuli <beleczky.gabor@liveedu.mathiasz.hu>
2026-04-19 23:21:20 +0200

    kisebb végpont változtatás a mostani frontendhez + bemutato profil + uj database export

--- Backend/app/Http/Controllers/CsoportTagsagController.php
+++ Backend/app/Http/Controllers/CsoportTagsagController.php
@@ -88,7 +88,7 @@ class CsoportTagsagController extends Controller
         $user = auth()->user();
         if ($user->jogosultsag_szint > 2)
         {
-            $resp = Csoportok::join("csoport_tagsag","csoport_tagsag.csoport_id","=","csoportok.id")->groupBy("csoportok.id","csoportok.megnevezes")->selectRaw("count(csoport_tagsag.id) as mennyiseg, csoportok.megnevezes")->get();
+            $resp = Csoportok::join("csoport_tagsag","csoport_tagsag.csoport_id","=","csoportok.id")->join("csoport_tipusok","csoportok.csoport_tipus_id","=","csoport_tipusok.id")->groupBy("csoportok.id","csoportok.megnevezes","csoport_tipusok.megnevezes")->selectRaw("count(csoport_tagsag.id) as mennyiseg, csoportok.megnevezes, csoportok.id, csoport_tipusok.megnevezes")->get();
             return response($resp);
         }
         else
